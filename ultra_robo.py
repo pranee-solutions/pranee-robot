a74c70b9175621ee2d643250a522264d78844ddc
git-prodigy <aaditya.prodigy@gmail.com>
2017-12-01 19:38:26 +0530

    Update ultra_robo.py
    Latest code .. ultrasonic sensor is not working need to chnage .. aways giving distance under zero

--- ultra_robo.py
+++ ultra_robo.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO  # Import GPIO library
 import time
-from gpiozero import DistanceSensor
+
+#from gpiozero import DistanceSensor
 # Import time library
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -13,7 +14,7 @@ RIGHT_MOTOR_2 = 22
 LEFT_MOTOR_1 = 24
 LEFT_MOTOR_2 = 23
 
-ultrasonic = DistanceSensor(echo=17, trigger=4)
+# ultrasonic = DistanceSensor(echo=17, trigger=4)
 # initialize GPIO Pin as input
 # GPIO.setup(led,GPIO.OUT)
 GPIO.setup(SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -21,12 +22,35 @@ GPIO.setup(RIGHT_MOTOR_1, GPIO.OUT)
 GPIO.setup(RIGHT_MOTOR_2, GPIO.OUT)
 GPIO.setup(LEFT_MOTOR_1, GPIO.OUT)
 GPIO.setup(LEFT_MOTOR_2, GPIO.OUT)
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
 # GPIO.output(led, 1)
 time.sleep(5)
 
 
+def calculateDistance():
+    avgDistance = 0
+    for i in range(5):
+        GPIO.output(TRIG, False)  # Set TRIG as LOW
+        time.sleep(0.1)  # Delay
+        GPIO.output(TRIG, True)  # Set TRIG as HIGH
+        time.sleep(0.00001)  # Delay of 0.00001 seconds
+        GPIO.output(TRIG, False)  # Set TRIG as LOW
+        pulse_start = time.time()
+        pulse_end = time.time()
+        pulse_duration = pulse_end - pulse_start  # time to get back the pulse to sensor
+        distance_l = pulse_duration * 17150  # Multiply pulse duration by 17150 (34300/2) to get distance
+        distance_l = round(distance_l, 2)  # Round to two decimal points
+        avgDistance = avgDistance + distance_l
+    avgDistance = avgDistance / 5
+    print "Distance coming as ", avgDistance
+    #sensor = DistanceSensor(echo=ECHO, trigger=TRIG)
+    #print "Distance: ", sensor.distance * 100
+    return avgDistance
+
+
 def stop():
-    #print "stop"
+    # print "stop"
     GPIO.output(RIGHT_MOTOR_1, 0)
     GPIO.output(RIGHT_MOTOR_2, 0)
     GPIO.output(LEFT_MOTOR_1, 0)
@@ -64,46 +88,48 @@ def right():
     GPIO.output(LEFT_MOTOR_2, 0)
     print "right"
 
-flag = 0
-count = 0
-while True:
+
+def turnRightOfLeft():
+    print "Stop called"
     stop()
-    j = GPIO.input(SWITCH)
-    print "press switch to start Robo"
-    if j == 1:  # Robot is activated when button is pressed
-        flag = 1
-        print "Robot Activated"
-        time.sleep(2)
-    while flag == 1:
-        distance = ultrasonic.distance * 100
-        distance = round(distance, 2)  # Round to two decimal points
-        count = 0
-        print "Distance coming as ", distance
-        if distance < 15:  # Check whether the distance is within 15 cm range
-            count = count + 1
-            print "Stop called"
-            stop()
-            time.sleep(1)
-            print "Back called"
-            back()
-            time.sleep(1.5)
-            if (count % 3 == 1) & (flag == 0):
-                print "Right called"
-                right()
+    time.sleep(1)
+    print "Back called"
+    back()
+    time.sleep(1.5)
+    print "Right called"
+    right()
+    time.sleep(1.5)
+    print "Stop called"
+    stop()
+    time.sleep(1)
+
+
+flag = 0
+try:
+    while True:
+        stop()
+        j = GPIO.input(SWITCH)
+        print "press switch to start Robo"
+        if j == 1:  # Robot is activated when button is pressed
+            flag = 1
+            print "Robot Activated", j
+
+        while flag == 1:
+            distance = calculateDistance()
+            if distance < 15:  # Obstacle detected
+                print "Obstacle detected"
+                turnRightOfLeft()
             else:
-                print "Left called"
-                left()
-            time.sleep(1.5)
-            print "Stop called"
-            stop()
-            time.sleep(1)
-        else:
-            print "Forward called"
-            forward()
+                time.sleep(0.5)
+                forward()
             j = GPIO.input(SWITCH)
             if j == 1:  # De activate robot on pushin the button
+                flag = 0
                 print "Robot De-Activated", j
                 stop()
                 time.sleep(1)
-                flag = 0
 
+except KeyboardInterrupt:
+    print("Measurement stopped by User")
+    stop()
+    GPIO.cleanup()
